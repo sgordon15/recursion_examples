45ac0ff75fc9b699d876456251b4ada01dcb6c28
kapst <kapster05@gmail.com>
2020-04-23 21:00:53 -0500

    all work

--- src/main/java/edu/ti/caih313/recursion/simple/RecursiveCountdown.java
+++ src/main/java/edu/ti/caih313/recursion/simple/RecursiveCountdown.java
@@ -1,16 +1,18 @@
 package edu.ti.caih313.recursion.simple;
 
 public class RecursiveCountdown {
-    public static void main(String[] args) {
-        countDown(3);
+    public static <String> void main(String[] args) {
+        countDown(3,">");
     }
 
-    public static void countDown(int num) {
+    public static void countDown(int num, String prefix) {
+        System.out.println(prefix + "Entering countDown with " +num);
         if (num == 0) {
-            System.out.println("That's all, folks!");
+            //System.out.println("That's all, folks!");
+            System.out.println( prefix +"done");
         } else {
-            System.out.print(num + " ");
-            countDown(num - 1);
+            System.out.print(prefix + " calling countDown with " + (num -1));
+            countDown(num - 1, "-" + prefix);
         }
     }
 }
